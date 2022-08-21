3186db82162391581ff675f1031053e59c8d8f67
santiago <santiagoneira52@gmail.com>
2022-08-21 03:01:59 -0300

    add icon to tab navigation

--- projects/create-screens-project/src/BarraNavegacion.js
+++ projects/create-screens-project/src/BarraNavegacion.js
@@ -2,6 +2,7 @@ import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 // importamos el envoltorio (wrapper de toda nuestra app)
 import { NavigationContainer } from "@react-navigation/native";
+import { Ionicons } from "react-native-vector-icons";
 
 // importamos los grupos de pantallas a manejar no pantallas individuales
 import GrupoHome from "../src/relacion-pantallas(stack)/GrupoHome";
@@ -14,12 +15,39 @@ const BarraNavegacion = () => {
   return (
     <NavigationContainer>
       <Tab.Navigator screenOptions={{ headerShown: false }}>
-        <Tab.Screen name="favoritos" component={GrupoFavoritos} />
-        <Tab.Screen name="home" component={GrupoHome} />
+        <Tab.Screen
+          name="favoritos"
+          component={GrupoFavoritos}
+          options={{
+            tabBarLabel: "Mi lista",
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="bookmark-outline" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="home"
+          component={GrupoHome}
+          options={{
+            tabBarLabel: "Inicio",
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="home-outline" color={color} size={size} />
+            ),
+          }}
+        />
         <Tab.Screen
           name="Mi cuenta"
           component={Cuenta}
-          options={{ headerShown: true }}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons
+                name="person-circle-outline"
+                color={color}
+                size={size}
+              />
+            ),
+            headerShown: true,
+          }}
         />
       </Tab.Navigator>
     </NavigationContainer>
